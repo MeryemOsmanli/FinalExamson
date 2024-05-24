d1409dad6a6b2448e3769579312e931e1e2a4f46
sabinabakirzada <bd6zz1ghz@code.edu.az>
2024-05-24 08:01:05 -0700

    yeni

---
+++ front/src/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./footer.scss"
+
+function Footer() {
+  return (
+    <div>
+      <div className="footer_back">
+        <div className="footer_container">
+          <p>©2018 All Rights Reserverd. This template is made with  by Colorlib</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
